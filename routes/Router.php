6ff36eb155fbc050c5e52cd180ef786b6c3db7c0
Aymane Benhima <aymanebenhima@gmail.com>
2023-12-27 22:09:33 +0100

    add post to router

--- routes/Router.php
+++ routes/Router.php
@@ -15,6 +15,10 @@ class Router {
         
     }
 
+    public function post(string $path, string $action) {
+        $this->routes['POST'][] = new Route($path, $action);
+    }
+
     public function run() {
         foreach($this->routes[$_SERVER['REQUEST_METHOD']] as $route) {
             if($route->matches($this->url)) {
