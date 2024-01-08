d49e4e991660c0f0014367132773bbbf6fc9b9f9
Aymane Benhima <aymanebenhima@gmail.com>
2024-01-08 11:50:58 +0100

    not found exceptions

---
+++ app/Exceptions/NotFoundException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Throwable;
+
+class NotFoundException extends Exception
+{
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function error404()
+    {
+        http_response_code(404);
+        require VIEWS . 'errors/404.php';
+    }
+    public function error500()
+    {
+        http_response_code(500);
+        require VIEWS . 'errors/404.php';
+    }
+}
