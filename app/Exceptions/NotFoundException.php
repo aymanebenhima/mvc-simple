e7689420898c21c150f334fd793033e2297d920b
Aymane Benhima <aymanebenhima@gmail.com>
2024-01-08 14:37:13 +0100

    fix not found exception

--- app/Exceptions/NotFoundException.php
+++ app/Exceptions/NotFoundException.php
@@ -7,19 +7,28 @@ use Throwable;
 
 class NotFoundException extends Exception
 {
+    /**
+     * Constructor for the class.
+     *
+     * @param string $message The error message.
+     * @param int $code The error code.
+     * @param Throwable|null $previous The previous exception.
+     * @throws Throwable
+     */
     public function __construct($message = "", $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
 
+    /**
+     * Renders a 404 error page.
+     *
+     * @throws None
+     * @return None
+     */
     public function error404()
     {
         http_response_code(404);
-        require VIEWS . 'errors/404.php';
-    }
-    public function error500()
-    {
-        http_response_code(500);
         require VIEWS . 'errors/404.php';
     }
 }
